cb11b640d3c63e5365872a73f929e56a4e7e1d22
eric <walkcloud@outlook.com>
2023-05-18 23:10:46 -0700

    fix checking jwtToken from cookie

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,16 +2,15 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { jwtConstants } from '../constants';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor() {
     super({
-      // jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       jwtFromRequest: ExtractJwt.fromExtractors([
-        (req) => {
-          return req?.cookies?.jwtToken;
-        },
+        JwtStrategy.extractJWT,
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
       ]),
       ignoreExpiration: false,
       secretOrKey: jwtConstants.secret,
@@ -20,5 +19,16 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
 
   async validate(payload: any) {
     return { userId: payload.sub, username: payload.username };
+  }
+
+  private static extractJWT(req: Request): string | null {
+    let token = null;
+    if (req.headers.cookie) {
+      const cookie = req.headers.cookie.split(';').find(cookie => cookie.trim().startsWith('jwtToken='));
+      if (cookie) {
+        token = cookie.split('=')[1];
+      }
+    }
+    return token;
   }
 }
