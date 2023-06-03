ffab241825e1d9d550fc6349c58a6eefebdecb74
eric <walkcloud@outlook.com>
2023-06-02 17:03:49 -0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     allowedHeaders: ['content-type'],
-    origin: '*',
+    origin: ['http://192.168.1.100:4000','http:localhost:4000'],
     credentials: true,
   });
   await app.listen(3000);
