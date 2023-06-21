837d486868f471a3599330e7628b31d079c06182
Eric <walkcloud@outlook.com>
2023-06-20 20:39:01 -0700

    delete list with its tasks

--- src/models/tasklists/tasklists.module.ts
+++ src/models/tasklists/tasklists.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tasklist } from './entities/tasklist.entity';
 import { TasklistsService } from './tasklists.service';
+import { Task } from '../tasks/entities/Task.entity';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Tasklist])],
+  imports:[TypeOrmModule.forFeature([Tasklist]), TypeOrmModule.forFeature([Task])],
   providers: [TasklistsService],
   exports: [TasklistsService],
 })
